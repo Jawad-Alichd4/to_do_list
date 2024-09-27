a02e9e82d5149b3b9865aa8e6625d8c9725ff6c8
Jawad-Alichd4 <159112594+Jawad-Alichd4@users.noreply.github.com>
2024-09-27 11:37:32 -0700

    updated script . js

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ function addTask() {
     var taskInput = document.getElementById("taskInput");
     var taskList = document.getElementById("taskList");
 
-var li = document.createElement("li");
+var li = document.createElement('li');
 li.textContent = taskInput.value;
-if (taskInput === "") {
-    alert("enter some text to add to list")
+if (taskInput.value === '') {
+   return alert("enter some text to add to list")
 
 }
 
-var removeButton = document.createElement("button");
-removeButton.textContent = "remove";
-removeButton.className = "remove-button";
-removeButton.onClick = function() {
+var removeButton = document.createElement('button');
+removeButton.textContent = 'remove';
+removeButton.className = 'remove-button';
+removeButton.onClick = function () {
     taskList.removeChild(li);
 
 };
 
-li.onClick = function() {
-    li.classList.toggle("completed");
+li.onClick = function () {
+    li.classList.toggle('completed');
 };
 
 li.appendChild(removeButton);
